56d23d6ca99c0639aa0dc8c3fe620dc9e34fce7a
Trevor Clarke <trevor.clarke62@gmail.com>
2021-03-28 15:50:48 -0400

    Create hello-jim_test.py

---
+++ hello-jim_test.py
@@ -0,0 +1,9 @@
+from hello_jim import hello_jim
+
+# Jim is a good guy, this should work.
+def test_one():
+    assert hello_jim(1) == "hello, Jim."
+
+# Sometimes I like to say hello to Jim many times.
+def test_two():
+    assert hello_jim(2) == "hello, hello, Jim."
