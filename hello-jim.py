3ec5a9c77e7cb2babfc01e6e478cdbfcb5f4e258
Trevor Clarke <trevor.clarke62@gmail.com>
2021-03-28 15:33:51 -0400

    Update and rename Hello-Jim_Sample-Code.py to hello-jim.py

--- hello-jim.py
+++ hello-jim.py
@@ -1,8 +1,8 @@
 def hello_jim(how_many):
     if how_many == 0:
-        return("Jim")
+        return("Jim.")
     else:
-        output = hello_jim(how_many-1) + " " + hello_jim(how_many-1)
+        output = "hello," + " " + hello_jim(how_many-1)
         print(output)
         return(output)
 hello_jim(5)
