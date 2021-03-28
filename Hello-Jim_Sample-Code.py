7086965cef238e4f42800b59dfaa19c38b03565e
Trevor Clarke <trevor.clarke62@gmail.com>
2021-03-28 15:26:22 -0400

    Create Hello-Jim_Sample-Code.py

---
+++ Hello-Jim_Sample-Code.py
@@ -0,0 +1,8 @@
+def hello_jim(how_many):
+    if how_many == 0:
+        return("Jim")
+    else:
+        output = hello_jim(how_many-1) + " " + hello_jim(how_many-1)
+        print(output)
+        return(output)
+hello_jim(5)
